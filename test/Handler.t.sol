bfdc5516856bfb5449cd72102689f58e9ccd8c63
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2023-09-25 16:32:51 +0700

    updated test

--- test/Handler.t.sol
+++ test/Handler.t.sol
@@ -3,51 +3,24 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Test.sol";
 import "src/Handler.sol";
-import "./mock/Mocked721.t.sol";
+import "./BaseTest.t.sol";
 
-contract HandlerTest is Test, Handler {
-    Mocked721 apes;
-    Mocked721 birds;
-
-    address constant account1 = address(1337);
-    address constant account2 = address(1339);
-
-    address apeAddress;
-    uint256 ape1Id;
-    uint256 ape2Id;
-    address birdAddress;
-    uint256 bird1Id;
-    uint256 bird2Id;
-    address[] collections;
-    uint256[] ids;
-
-    function setUp() public {
-        apes = new Mocked721("Apes", "APE");
-        birds = new Mocked721("Birds", "BIRD");
-
-        apeAddress = address(apes);
-        ape1Id = 1;
-        ape2Id = 2;
-        birdAddress = address(birds);
-        bird1Id = 1;
-        bird2Id = 2;
-    }
-
+contract HandlerTest is BaseTest, Handler {
     function testIdsLonger() public {
-        collections.push(apeAddress);
-        ids.push(ape1Id);
-        ids.push(ape2Id);
+        creator721Collections.push(apeAddress);
+        creator721Ids.push(ape1Id);
+        creator721Ids.push(ape2Id);
 
         vm.expectRevert(LengthMismatch.selector);
-        _transferTokens(collections, ids, account1, account2);
+        _transferTokens(creator721Collections, creator721Ids, account1, account2);
     }
 
     function testCollectionsLonger() public {
-        collections.push(apeAddress);
-        collections.push(apeAddress);
-        ids.push(ape1Id);
+        creator721Collections.push(apeAddress);
+        creator721Collections.push(apeAddress);
+        creator721Ids.push(ape2Id);
 
         vm.expectRevert(LengthMismatch.selector);
-        _transferTokens(collections, ids, account1, account2);
+        _transferTokens(creator721Collections, creator721Ids, account1, account2);
     }
 }
