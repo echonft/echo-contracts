eb327fb2c0b80e7a3e937d9536e7f887bac22c41
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-02-20 13:44:14 +0700

    improved script for easier use

--- script/distribute-nfts.sh
+++ script/distribute-nfts.sh
@@ -2,46 +2,58 @@
 
 source ../.env
 
-receiver=0x65A4089F413503742628DE0FbC349f2Fa3eC0440
+# Constants
+bayc=0x65426F3C04e85936b0F875510d045b413134186A
+mayc=0xd91303F46C3f4883D9D74c703C15948e5E04E110
+cyberkongz=0x43bE93945E168A205D708F1A41A124fA302e1f76
+sora=0x3512BbCAD47275F8e11e7B843FA4BDf7b526f5b8
+creepz=0xB9FBf3cDf5B344a9e09528FEe853e376d57eE7cA
+nftsToSend=2
+
+# Addresses
+receiver=0xE2479c6935cd881C550D69D75d13113833a91B2E
 sender=0x213bE2f484Ab480db4f18b0Fe4C38e1C25877f09
 
-bayc=0x65426F3C04e85936b0F875510d045b413134186A
-idsBayc=(90 91)
-mayc=0xd91303F46C3f4883D9D74c703C15948e5E04E110
-idsMayc=(75 76)
-cyberkongz=0x43bE93945E168A205D708F1A41A124fA302e1f76
-idsKongz=(65 66)
-sora=0x3512BbCAD47275F8e11e7B843FA4BDf7b526f5b8
-idsSora=(43 44)
-creepz=0xB9FBf3cDf5B344a9e09528FEe853e376d57eE7cA
-idsCreepz=(43 44)
+# IDs
+baycStartingId=104
+maycStartingId=89
+kongzStartingId=79
+soraStartingId=59
+creepzStartingId=59
 
 # Distribute BAYC
-for id in "${idsBayc[@]}"
+for((id=baycStartingId;id<baycStartingId + nftsToSend; id++))
 do
     cast send $bayc "transferFrom(address, address, uint256)" $sender $receiver $id --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY
 done
 
 # Distribute MAYC
-for id in "${idsMayc[@]}"
+for((id=maycStartingId;id<maycStartingId + nftsToSend; id++))
 do
     cast send $mayc "transferFrom(address, address, uint256)" $sender $receiver $id --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY
 done
 
 # Distribute Cyberkongz
-for id in "${idsKongz[@]}"
+for((id=kongzStartingId;id<kongzStartingId + nftsToSend; id++))
 do
     cast send $cyberkongz "transferFrom(address, address, uint256)" $sender $receiver $id --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY
 done
 
 # Distribute Soras
-for id in "${idsSora[@]}"
+for((id=soraStartingId;id<soraStartingId + nftsToSend; id++))
 do
     cast send $sora "transferFrom(address, address, uint256)" $sender $receiver $id --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY
 done
 
 # Distribute Creepz
-for id in "${idsCreepz[@]}"
+for((id=creepzStartingId;id<creepzStartingId + nftsToSend; id++))
 do
     cast send $creepz "transferFrom(address, address, uint256)" $sender $receiver $id --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY
 done
+
+echo "New starting ids:"
+echo "BAYC:$((baycStartingId + nftsToSend))"
+echo "MAYC:$((maycStartingId + nftsToSend))"
+echo "Cyberkongz:$((kongzStartingId + nftsToSend))"
+echo "Soras:$((soraStartingId + nftsToSend))"
+echo "Creepz:$((creepzStartingId + nftsToSend))"
