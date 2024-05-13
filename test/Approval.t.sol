200bc2e763eb14cf1ff44a03466d850a6fcc46f9
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-05-13 14:50:48 -0400

    added more tests

--- test/Approval.t.sol
+++ test/Approval.t.sol
@@ -6,91 +6,70 @@ import "./mock/Mocked721.sol";
 import "forge-std/Test.sol";
 
 contract ApprovalTest is BaseTest {
-// Creator has not approved its apes
-//    function testCannotExecuteTradeIfCreatorDidNotApprove() public {
-//        creator721Collections.push(apeAddress);
-//        creator721Ids.push(ape3Id);
-//        counterparty721Collections.push(birdAddress);
-//        counterparty721Ids.push(bird1Id);
-//
-//        Trade memory trade = Trade({
-//            id: "test",
-//            creator: account3,
-//            counterparty: account2,
-//            expiresAt: in6hours,
-//            creatorCollections: creator721Collections,
-//            creatorIds: creator721Ids,
-//            counterpartyCollections: counterparty721Collections,
-//            counterpartyIds: counterparty721Ids
-//        });
-//
-//        (uint8 vSigner, bytes32 rSigner, bytes32 sSigner, Signature memory signature) =
-//            _prepareSignature(trade, account2PrivateKey, signerPrivateKey);
-//        vm.prank(account3);
-//        vm.expectRevert(bytes("NOT_AUTHORIZED"));
-//        echo.executeTrade(vSigner, rSigner, sSigner, signature, trade);
-//    }
-//
-//    /// Counterparty has not approved its birds
-//    function testCannotExecuteTradeIfCounterpartyDidNotApprove() public {
-//        creator721Collections.push(apeAddress);
-//        creator721Ids.push(ape1Id);
-//        counterparty721Collections.push(birdAddress);
-//        counterparty721Ids.push(bird1Id);
-//
-//        Trade memory trade = Trade({
-//            id: "test",
-//            creator: account1,
-//            counterparty: account2,
-//            expiresAt: in6hours,
-//            creatorCollections: creator721Collections,
-//            creatorIds: creator721Ids,
-//            counterpartyCollections: counterparty721Collections,
-//            counterpartyIds: counterparty721Ids
-//        });
-//
-//        vm.startPrank(account2);
-//        birds.setApprovalForAll(address(echo), false);
-//        vm.stopPrank();
-//
-//        (uint8 vSigner, bytes32 rSigner, bytes32 sSigner, Signature memory signature) =
-//            _prepareSignature(trade, account2PrivateKey, signerPrivateKey);
-//        vm.prank(account1);
-//        vm.expectRevert(bytes("NOT_AUTHORIZED"));
-//        echo.executeTrade(vSigner, rSigner, sSigner, signature, trade);
-//    }
-//
-//    /// Succeeds when both creator and counterparty have approved their assets
-//    function testSucceedsWhenApproved() public {
-//        // Assets are in original state
-//        assertEq(apes.ownerOf(1), account1);
-//        assertEq(birds.ownerOf(1), account2);
-//
-//        creator721Collections.push(apeAddress);
-//        creator721Ids.push(ape1Id);
-//        counterparty721Collections.push(birdAddress);
-//        counterparty721Ids.push(bird1Id);
-//
-//        Trade memory trade = Trade({
-//            id: "test",
-//            creator: account1,
-//            counterparty: account2,
-//            expiresAt: in6hours,
-//            creatorCollections: creator721Collections,
-//            creatorIds: creator721Ids,
-//            counterpartyCollections: counterparty721Collections,
-//            counterpartyIds: counterparty721Ids
-//        });
-//
-//        (uint8 vSigner, bytes32 rSigner, bytes32 sSigner, Signature memory signature) =
-//            _prepareSignature(trade, account2PrivateKey, signerPrivateKey);
-//        vm.prank(account1);
-//        vm.expectEmit(true, true, true, true);
-//        emit TradeExecuted("test");
-//        echo.executeTrade(vSigner, rSigner, sSigner, signature, trade);
-//
-//        // Assets are now swapped
-//        assertEq(apes.ownerOf(1), account2);
-//        assertEq(birds.ownerOf(1), account1);
-//    }
+    function testCannotExecuteTradeIfSenderDidNotApprove() public {
+        address[] memory senderTokenAddresses = new address[](1);
+        senderTokenAddresses[0] = apeAddress;
+        uint256[] memory senderTokenIds = new uint256[](1);
+        senderTokenIds[0] = ape3Id;
+
+        address[] memory receiverTokenAddresses = new address[](1);
+        receiverTokenAddresses[0] = birdAddress;
+        uint256[] memory receiverTokenIds = new uint256[](1);
+        receiverTokenIds[0] = bird1Id;
+
+        Offer memory offer = generateOffer(
+            account3,
+            senderTokenAddresses,
+            senderTokenIds,
+            block.chainid,
+            account2,
+            receiverTokenAddresses,
+            receiverTokenIds,
+            block.chainid,
+            in6hours,
+            OfferState.OPEN
+        );
+
+        vm.prank(account3);
+        vm.expectRevert(bytes("NOT_AUTHORIZED"));
+        echo.createOffer(offer);
+    }
+
+    function testCannotExecuteTradeIfReceiverDidNotApprove() public {
+        address[] memory senderTokenAddresses = new address[](1);
+        senderTokenAddresses[0] = apeAddress;
+        uint256[] memory senderTokenIds = new uint256[](1);
+        senderTokenIds[0] = ape1Id;
+
+        address[] memory receiverTokenAddresses = new address[](1);
+        receiverTokenAddresses[0] = birdAddress;
+        uint256[] memory receiverTokenIds = new uint256[](1);
+        receiverTokenIds[0] = bird1Id;
+
+        Offer memory offer = generateOffer(
+            account1,
+            senderTokenAddresses,
+            senderTokenIds,
+            block.chainid,
+            account2,
+            receiverTokenAddresses,
+            receiverTokenIds,
+            block.chainid,
+            in6hours,
+            OfferState.OPEN
+        );
+
+        vm.prank(account1);
+        echo.createOffer(offer);
+
+        vm.startPrank(account2);
+        birds.setApprovalForAll(address(echo), false);
+        vm.stopPrank();
+
+        bytes32 offerId = generateOfferId(offer);
+
+        vm.prank(account2);
+        vm.expectRevert(bytes("NOT_AUTHORIZED"));
+        echo.acceptOffer(offerId);
+    }
 }
