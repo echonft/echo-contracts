9eeb04ce808d28e8bbcf232a5a5eac10b494baca
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-05-14 16:30:34 -0400

    updated scripts

--- script/DeployEcho.s.sol
+++ script/DeployEcho.s.sol
@@ -9,7 +9,8 @@ contract DeployEcho is Script {
     function test() public {}
 
     function run() external {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
         new Echo({owner: address(0x213bE2f484Ab480db4f18b0Fe4C38e1C25877f09)});
 
         vm.stopBroadcast();
