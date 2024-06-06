9f076f30b01ada79b1e1f24f1f411189d9af0b20
Gabriel Cartier <4060669+GabrielCartier@users.noreply.github.com>
2024-06-05 23:39:08 -0400

    fixed scripts

--- script/DeployEchoBlastMainnet.s.sol
+++ script/DeployEchoBlastMainnet.s.sol
@@ -5,21 +5,21 @@ import "forge-std/Script.sol";
 import "../src/EchoBlast.sol";
 import "../test/mock/YieldMock.sol";
 
-contract DeployEchoBlast is Script {
+contract DeployEchoBlastMainnet is Script {
     // Exclude from coverage report
     function test() public {}
 
     function run() external {
-        vm.createSelectFork(vm.envString("BLAST_SEPOLIA_RPC_URL"));
+        vm.createSelectFork(vm.envString("BLAST_RPC_URL"));
         // Deploy mock of the precompile
         YieldMock yieldMock = new YieldMock();
         // Set mock bytecode to the expected precompile address
         vm.etch(0x0000000000000000000000000000000000000100, address(yieldMock).code);
 
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("MAINNET_PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
-        new EchoBlast({owner: deployer, blastPointsAddress: address(0x2fc95838c71e76ec69ff817983BFf17c710F34E0)});
+        new EchoBlast({owner: deployer, blastPointsAddress: address(0x2536FE9ab3F511540F2f9e2eC2A805005C3Dd800)});
 
         vm.stopBroadcast();
     }
--- script/UpdateEchoFees.s.sol
+++ script/UpdateEchoFees.s.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
-import "../src/Echo.sol";
+import "../src/EchoBlast.sol";
 
 contract UpdateEchoFees is Script {
     // Exclude from coverage report
     function test() public {}
 
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("MAINNET_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        Echo echo = Echo(0xB0904D81440EFCA27Ec61948c95f21D7d546F8C3);
+        EchoBlast echo = EchoBlast(0x538dD3e75d05B63dc81FEe587B8a4AA5Fde2cc95);
         echo.setFees(0.005 ether);
         vm.stopBroadcast();
     }
